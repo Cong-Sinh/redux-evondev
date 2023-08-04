b699e489abae953904a061daa94c6ef8981d0a59
Macbook <macbook@MacBook-Pro-cua-Sinhdz.local>
2023-08-04 18:05:12 +0700

    add: dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from "react-redux";
+import Card from "./components/Card";
+import Counter from "./components/Counter";
 
 function App() {
+  const globalOptions = useSelector((state) => state.global);
+  console.log("App ~ globalOptions", globalOptions);
+  // const handleToggleDarkmode = () => {
+  //   dispatch(toggleDarkMode(true));
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card></Card>
+      <Counter></Counter>
     </div>
   );
 }
